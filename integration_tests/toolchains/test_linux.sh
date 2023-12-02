24507b47adcd03fa1a13157cee67d72d5db44214
Alexander Batashev <alexbatashev@outlook.com>
2023-12-02 18:33:18 +0300

    fixes for toolchain

--- integration_tests/toolchains/test_linux.sh
+++ integration_tests/toolchains/test_linux.sh
@@ -5,9 +5,9 @@ set -eu
 bazel build //...
 bazel run //:test
 
-bazel run --crosstool_top=@clang//:toolchain //:test
-bazel run --crosstool_top=@clang//:toolchain -c dbg //:test
-bazel run --crosstool_top=@clang//:toolchain -c opt //:test
-bazel run --crosstool_top=@clang//:toolchain --features=extra_warnings --features=werror //:test
-bazel run --crosstool_top=@clang//:toolchain --features=c++20 //:test
-bazel run --crosstool_top=@clang//:toolchain --features=static_link_cpp_runtimes //:test
+bazel run --crosstool_top=@llvm-17//:toolchain //:test
+bazel run --crosstool_top=@llvm-17//:toolchain -c dbg //:test
+bazel run --crosstool_top=@llvm-17//:toolchain -c opt //:test
+bazel run --crosstool_top=@llvm-17//:toolchain --features=extra_warnings --features=werror //:test
+bazel run --crosstool_top=@llvm-17//:toolchain --features=c++20 //:test
+bazel run --crosstool_top=@llvm-17//:toolchain --features=static_link_cpp_runtimes //:test
