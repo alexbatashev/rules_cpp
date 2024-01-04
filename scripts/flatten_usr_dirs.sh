e13a35f711f677dcd76ab72a2a9a008eb9d13396
Alexander Batashev <alexbatashev@outlook.com>
2024-01-04 22:10:26 +0300

    try a different prefix

--- scripts/flatten_usr_dirs.sh
+++ scripts/flatten_usr_dirs.sh
@@ -3,10 +3,10 @@
 set -eu
 
 if [ -d "$1/usr" ]; then
-    for dir in "$1/usr/*/"
+    for dir in $1/usr/local/*/
     do
         dir=${dir%*/}
-        mv $dir "$1/${dir##*/}"
+        mv $dir $1/${dir##*/}
     done
 
     rm -r $1/usr
