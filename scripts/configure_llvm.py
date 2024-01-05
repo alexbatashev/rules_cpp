dcd650c18db82cf696a866dcb9262e85dc2a7b7c
Alexander Batashev <alexbatashev@outlook.com>
2024-01-05 13:42:27 +0300

    more apple stuff

--- scripts/configure_llvm.py
+++ scripts/configure_llvm.py
@@ -103,7 +103,7 @@ if not args.target_cpu.startswith("x86_64"):
     if platform.system() == "Darwin":
         cmake_args.extend([
             "-DCMAKE_OSX_ARCHITECTURES=arm64",
-            "-DCMAKE_SYSTEM_NAME=Apple",
+            "-DCMAKE_SYSTEM_NAME=Apple-Clang",
             "-DCMAKE_SYSTEM_PROCESSOR=arm64"
         ])
     elif platform.system() == "Linux":
